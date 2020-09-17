315aa3bb5a9e704a210be07142ad94dfa716709f
Evan Leon Tilley <elt2141@columbia.edu>
2020-09-17 16:04:44 -0400

    made some updates to synthesis section

--- script.js
+++ script.js
@@ -9,4 +9,31 @@ $(document).ready(function()
 			$(this).addClass("fa-chevron-down")
 		}
 	})
+	$("#collapse2").click(function(){
+		if ($(this).hasClass("fa-chevron-down")){
+			$(this).removeClass("fa-chevron-down")
+			$(this).addClass("fa-chevron-up")
+		} else {
+			$(this).removeClass("fa-chevron-up")
+			$(this).addClass("fa-chevron-down")
+		}
+	})
+	$("#collapse3").click(function(){
+		if ($(this).hasClass("fa-chevron-down")){
+			$(this).removeClass("fa-chevron-down")
+			$(this).addClass("fa-chevron-up")
+		} else {
+			$(this).removeClass("fa-chevron-up")
+			$(this).addClass("fa-chevron-down")
+		}
+	})
+	$("#collapse4").click(function(){
+		if ($(this).hasClass("fa-chevron-down")){
+			$(this).removeClass("fa-chevron-down")
+			$(this).addClass("fa-chevron-up")
+		} else {
+			$(this).removeClass("fa-chevron-up")
+			$(this).addClass("fa-chevron-down")
+		}
+	})
 })
